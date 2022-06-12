f4d9a74818c2298cb024e85e0a0343fd06a50d10
Henrique <henrique.asouza@pm.me>
2022-06-12 18:18:37 -0300

    chore: code improve

--- Perceptron.py
+++ Perceptron.py
@@ -4,11 +4,13 @@ from sklearn.metrics import accuracy_score
 from data.DataAnalises import X_treated, y_treated, X_non_treated, y_non_treated
 from utils.nonTreatedDataUtils import indexConvert
 from utils.treatedDataUtils import indexConvert as indexConvert_treated
+import time
 
 
 def formatAccuracy(y_t, y_p):
     return "{:.2f}".format(accuracy_score(y_t, y_p) * 100)
 
+start = time.time()
 
 # NOTE - Treinando o modelo sem tratamento
 X_non_treated = indexConvert(X_non_treated)
@@ -23,24 +25,7 @@ y_pred_non_treated = clf_non_treated.predict(x_test_non_treated)
 accuracy_non_treated = formatAccuracy(
     y_test_non_treated, y_pred_non_treated).__str__()
 
-'''
-Testando o KNN com dados de uma pessoa
-Idade: 35
-Flng-Wgt: 106967
-Workclass: Private
-Education: Bachelors
-Education-Num: 11
-Marital Status: Married-civ-spouse
-Occupation: Prof-specialty
-Relationship: Husband
-Race: White
-Sex: Male
-Captail-gain: 0
-Capitasl-los: 0
-Hours-per-week: 40
-Native-country: United-States
-'''
-example_non_treated = [35, 106967, 0, 0, 11, 0, 5, 2, 0, 1, 0, 0, 40, 0]
+example_non_treated = [35, 0, 106967, 0, 11, 0, 5, 2, 0, 1, 3000, 0, 40, 0]
 person_data_non_treated = clf_non_treated.predict([example_non_treated])
 
 
@@ -56,12 +41,14 @@ clf_treated.fit(x_train_treated, y_train_treated)
 y_pred_treated = clf_treated.predict(x_test_treated)
 accuracy_treated = formatAccuracy(y_test_treated, y_pred_treated).__str__()
 
-# example_treated = [35, 0, 0, 0, 5, 2, 0, 1, 0, 0, 40, 0]
 example_treated = [1, 0, 0, 0, 5, 2, 0, 1, 0, 0, 1]
 person_data_treated = clf_treated.predict([example_treated])
+
+finish = time.time()
 
 # NOTE - Printando os resultados
 print('Acurácia: \n' + 'Antes: ' + accuracy_non_treated +
       '%\n' + 'Depois: ' + accuracy_treated + '%')
 print('Predição de income:\n' + 'Antes: ' + person_data_non_treated[0].__str__(
 ) + ' dólares /ano\n' + 'Depois: ' + person_data_treated[0].__str__() + ' dólares /ano')
+print('\nTempo de execução: ' + "{:.2f}".format(finish - start) + ' segundos')
--- PerceptronClean.py
+++ PerceptronClean.py
@@ -6,6 +6,7 @@ from utils.nonTreatedDataUtils import indexConvert
 from utils.treatedDataUtils import indexConvert as indexConvert_treated
 import pandas as pd
 import pickle
+import time
 model_non_treated = 'models/nonTreatedModel.sav'
 model_treated = 'models/treatedModel.sav'
 
@@ -13,6 +14,7 @@ model_treated = 'models/treatedModel.sav'
 def formatAccuracy(y_t, y_p):
     return "{:.2f}".format(accuracy_score(y_t, y_p) * 100)
 
+start = time.time()
 
 # NOTE - Treinando o modelo sem tratamento
 X_non_treated = indexConvert(X_non_treated)
@@ -31,7 +33,7 @@ y_pred_non_treated = loaded_non_treated_model.predict(x_test_non_treated)
 accuracy_non_treated = formatAccuracy(
     y_test_non_treated, y_pred_non_treated).__str__()
 
-example_non_treated = [35, 106967, 0, 0, 11, 0, 5, 2, 0, 1, 0, 0, 40, 0]
+example_non_treated = [35, 0, 106967, 0, 11, 0, 5, 2, 0, 1, 3000, 0, 40, 0]
 person_data_non_treated = loaded_non_treated_model.predict(
     [example_non_treated])
 
@@ -53,12 +55,14 @@ loaded_treated_model = pickle.load(open(model_treated, 'rb'))
 y_pred_treated = loaded_treated_model.predict(x_test_treated)
 accuracy_treated = formatAccuracy(y_test_treated, y_pred_treated).__str__()
 
-# example_treated = [35, 0, 0, 0, 5, 2, 0, 1, 0, 0, 40, 0]
 example_treated = [1, 0, 0, 0, 5, 2, 0, 1, 0, 0, 1]
 person_data_treated = loaded_treated_model.predict([example_treated])
+
+finish = time.time()
 
 # NOTE - Printando os resultados
 print('Acurácia: \n' + 'Antes: ' + accuracy_non_treated +
       '%\n' + 'Depois: ' + accuracy_treated + '%')
 print('Predição de income:\n' + 'Antes: ' + person_data_non_treated[0].__str__(
 ) + ' dólares /ano\n' + 'Depois: ' + person_data_treated[0].__str__() + ' dólares /ano')
+print('\nTempo de execução: ' + "{:.2f}".format(finish - start) + ' segundos')
